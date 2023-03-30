2d9c7c1fb50328e0e8e941618914b3d3806b8607
RustFisher <rustfisher@foxmail.com>
2023-03-30 14:57:53 +0800

    auto commit

---
+++ about/baidu.js
@@ -0,0 +1,22 @@
+// 百度统计
+var _hmt = _hmt || [];
+  (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?9a99e4ef18a8c3f13a014114d842cd1e";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+  })();
+
+// 自动推送百度爬虫
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
